4c53285c3f3b6fabc50eaed6fbf93c2080d4f9d9
malmgren80 <marcusmalmgren@hotmail.com>
2014-01-31 16:33:12 +0100

    Removed unused using

--- source/Glimpse.Ado/Tab/Sql.cs
+++ source/Glimpse.Ado/Tab/Sql.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Runtime.Serialization.Formatters;
 using Glimpse.Ado.Message;
 using Glimpse.Ado.Model;
 using Glimpse.Ado.Tab.Support;
